b8b2b82291353a7bbe06263c4e3ae335316b0f64
pnanstasia <116551880+pnanstasia@users.noreply.github.com>
2023-02-20 16:41:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-
+def read_file(path_to_file, year):
+    """
+    Function for reading file
+    """
+    result = []
+    with open(path_to_file, encoding='utf-8') as file:
+        work = file.readlines()
+        for lines in work:
+            if lines.startswith('"'):
+                result.append(lines.strip('\n').split('\t'))
+    work_list = []
+    for elements in result:
+        temp = []
+        for value in elements:
+            if (value != '') and (not value.startswith('(')):
+                temp.append(value)
+        work_list.append(temp)
+    fitable_films = []
+    for i in work_list:
+        if str(year) in i[0]:
+            fitable_films.append(i)
+    return fitable_films
